4b5593320105d1edbf1525194dc0488d7aea9f99
cjw4458 <cjw1234577@gmail.com>
2022-11-05 18:18:05 +0900

    First commit!

---
+++ database/login.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "login" (
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"user_account"	TEXT NOT NULL UNIQUE,
+	"user_password"	TEXT,
+	"user_name"	TEXT NOT NULL,
+	"user_authority"	INTEGER NOT NULL,
+	"social_login_id"	TEXT,
+	"social_login_secret"	TEXT,
+	"access_token"	TEXT,
+	PRIMARY KEY("user_id" AUTOINCREMENT)
+);
+COMMIT;
